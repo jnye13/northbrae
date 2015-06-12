251c2a10ef30c0e660851ac7e402e741195ba91f
Felix Hedlund <felixhedlund@me.com>
2015-06-12 13:12:43 -0700

    explaining text for requestDemoMailer

--- app/mailers/request_demo_mailer.rb
+++ app/mailers/request_demo_mailer.rb
@@ -1,8 +1,7 @@
 class RequestDemoMailer < ActionMailer::Base
   default :from => "SMRT Signup <notifier@smrtsystems.com>"
-	 #Just for test - to be replaced by:
-    #"admin@smrtsystems.com, bill@smrtsystems.com"
-  
+
+  #Sends automatic POST to Close.io and MailChimp
   def request_demo_email(request_demo)
     @request_demo = request_demo
     
@@ -29,11 +28,11 @@ class RequestDemoMailer < ActionMailer::Base
 	    custom: custom
   	}
 
-	auth = {:username => "b4ce5c1ca62fbf4e68e9f504e29c072029f9a31f84cc35b1c4383f6c", :password => ""}
+	authCloseIO = {:username => "b4ce5c1ca62fbf4e68e9f504e29c072029f9a31f84cc35b1c4383f6c", :password => ""}
 
 	HTTParty.post("https://app.close.io/api/v1/lead/",
     :body => queryCloseIO.to_json,
-    :basic_auth => auth,
+    :basic_auth => authCloseIO,
     :verify => false,
     :headers => {
     	'Content-Type' => 'application/json'})
@@ -43,11 +42,20 @@ class RequestDemoMailer < ActionMailer::Base
 	merge_fields["FNAME"] = "#{@request_demo[:firstName]}"
 	merge_fields["LNAME"] = "#{@request_demo[:lastName]}"
 
+	authMailChimp = {:username => "", :password => "15012d12692466fe49b2aa3dea3c174e-us10"}
+
 	queryMailChimp = {
 	    email_address: "#{@request_demo[:email]}",
 	    status: "subscribed",
 	    merge_fields: merge_fields
   	}
 	
+  	HTTParty.post("https://us10.api.mailchimp.com/3.0/lists/29a85dfb26/members/", 
+  		:body => queryMailChimp.to_json,
+  		:basic_auth => authMailChimp,
+  		:headers => {
+    	'Content-Type' => 'application/json'})
+  	)
+
   end
 end
