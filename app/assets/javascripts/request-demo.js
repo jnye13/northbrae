7820d4b25c36bab0f412d83537f7fb8717fafbf0
Felix Hedlund <felixhedlund@me.com>
2015-06-15 10:27:58 -0700

    request demo modals fixed

--- app/assets/javascripts/request-demo.js
+++ app/assets/javascripts/request-demo.js
@@ -1,62 +1,5 @@
 $(document).on("page:change",function(){
-	var isVisible = $( "#requestDemo" ).is( ":visible" );
-	$('#btn-request-demo').click(function(){
-		$('#requestDemo').toggle('fast',function(){
-			if(isVisible){
-				isVisible = false;
-			}else{
-				isVisible = true;
-			}
-			$(this).find('#signup_name').focus();
-		});
-	});
-	function closeSignup(){
-  		$('#requestDemo').toggle('fast',function(){});
-	}
-	var $cancel = $('#btn-signup-cancel').click(closeSignup);
-	$('#btn-signup-done').click(closeSignup);
 
-	var $requestDemo = $('#requestDemo');
-
-	$('#requestDemoForm').bind('submit', function(ev){
-
-		ev.preventDefault();
-
-		var $this = $(this),
-			$progress = $requestDemo.find('.progress');
-			$progress.css('visibility','visible'),
-			$requestDemoFormTab = $requestDemo.find('.requestDemoFormTab'),
-			$requestDemoThanks = $requestDemo.find('.requestDemoThanks'),
-			$wrapper = $requestDemo.children().first(),
-			$submit = $this.find('[type=submit]');
-		
-		if($submit.hasClass('disabled')){
-			return;
-		}
-		
-		$submit.addClass('disabled');
-		$cancel.addClass('disabled');
-		
-		$.ajax({type: "POST", url: $this.attr('action'), data: $this.serialize(), success: function(){
-			$requestDemoFormTab.animate({'margin-left':'-355px'},function(){});
-			$requestDemoThanks.animate({'margin-left':'0px'});
-			$wrapper.animate({'height':'126px'});
-		}});
-	});
-
-	
-	$('body').click(function(evt){
-		if(isVisible){
-			console.log(evt.target.id);
-	       	if($(evt.target).closest('#requestDemo').length)
-	          	return; 
-	        if($(evt.target).closest('.btn-cta-secondary').length)
-	      		return;
-
-			$('#requestDemo').toggle('fast');
-			isVisible = false;
-		}
-	});
 
 	function in_progress_callback() {
     	console.log("in_progress_callback")
@@ -87,6 +30,25 @@ $(document).on("page:change",function(){
        success: success_callback,         // called when validator has returned
        error: validation_error           // called when an error reaching the validator has occured
    });
+
+  $('#requestDemoForm').submit(function(ev){
+      ev.preventDefault();
+      var $this = $(this),
+        $submit = $this.find('[type=submit]');
+      
+      if($submit.hasClass('disabled')){
+        return;
+      }
+      
+      $submit.addClass('disabled');
+      
+      $.ajax({type: "POST", url: $this.attr('action'), data: $this.serialize(), success: function(){
+        $('#requestDemoModal').modal('hide')
+        $('#thankYouModal').modal('show');
+      }});
+    });
+
+
 });
 
 
