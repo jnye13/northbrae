9bd637b85f6a206c0b7e824eed6ed9729a4a6ed7
Johan Baversjo <baversjo@gmail.com>
2015-10-28 17:50:31 -0700

    new website

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,136 +1,66 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require twitter/bootstrap
-//= require_tree .
+//= require bootstrap.min
+//= require cbpAnimatedHeader
+//= require classie
+//= require froogaloop2.min
+//= require jquery.easing.min
+//= require jquery.fittext
+//= require jquery.flexslider-min
+//= require wow.min
+//= require creative
 //= require_self
 
+$(function() {
+     // The slider being synced must be initialized first
+     if($('body').hasClass('index')){
 
-$(document).ready(function() {
+	     // Vimeo API nonsense
+	      var player1 = document.getElementById('player_1');
+	      $f(player1).addEvent('ready', ready);
 
-    /* ======= jQuery FitVids - Responsive Video ======= */
-    /* Ref: https://github.com/davatron5000/FitVids.js/blob/master/README.md */
-    
-    $(".video-container").fitVids();
+	      var player2 = document.getElementById('player_2');
+	      $f(player2).addEvent('ready', ready);
 
+	      var player3 = document.getElementById('player_3');
+	      $f(player3).addEvent('ready', ready);
 
-        /* ======= Twitter Bootstrap hover dropdown ======= */   
-    /* Ref: https://github.com/CWSpear/bootstrap-hover-dropdown */ 
-    /* apply dropdownHover to all elements with the data-hover="dropdown" attribute */
-    
-    $('[data-hover="dropdown"]').dropdownHover();
-    
-    /* ======= Fixed header when scrolled ======= */    
-    $(window).bind('scroll', function() {
-         
-         if ($(window).scrollTop() > 0) {
-             $('#header').addClass('scrolled');
-         }
-         else {
-             $('#header').removeClass('scrolled');
-             
-         }
-    });
-    
-    /* ======= jQuery Placeholder ======= */
-    /* Ref: https://github.com/mathiasbynens/jquery-placeholder */
-    
-    $('input, textarea').placeholder();    
-    
-    /* ======= FAQ accordion ======= */
-    function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find('.panel-title a')
-        .toggleClass('active')
-        .find("i.fa")
-        .toggleClass('fa-plus-square fa-minus-square');
-    }
-    $('.panel').on('hidden.bs.collapse', toggleIcon);
-    $('.panel').on('shown.bs.collapse', toggleIcon);    
-    
-    
-    /* ======= Header Background Slideshow - Flexslider ======= */    
-    /* Ref: https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties */
-    
-    $('.bg-slider').flexslider({
-        animation: "fade",
-        directionNav: false, //remove the default direction-nav - https://github.com/woothemes/FlexSlider/wiki/FlexSlider-Properties
-        controlNav: false, //remove the default control-nav
-        slideshowSpeed: 8000
-    });
-     
-    
-     /* ======= Testimonial Bootstrap Carousel ======= */
-     /* Ref: http://getbootstrap.com/javascript/#carousel */
-    $('#testimonials-carousel').carousel({
-      interval: 8000 
-    });
-    
-    
-    /* ======= Style Switcher ======= */    
-    $('#config-trigger').on('click', function(e) {
-        var $panel = $('#config-panel');
-        var panelVisible = $('#config-panel').is(':visible');
-        if (panelVisible) {
-            $panel.hide();          
-        } else {
-            $panel.show();
-        }
-        e.preventDefault();
-    });
-    
-    $('#config-close').on('click', function(e) {
-        e.preventDefault();
-        $('#config-panel').hide();
-    });
-    
-    
-    $('#color-options a').on('click', function(e) { 
-        var $styleSheet = $(this).attr('data-style');
-		$('#theme-style').attr('href', $styleSheet);	
-				
-		var $listItem = $(this).closest('li');
-		$listItem.addClass('active');
-		$listItem.siblings().removeClass('active');
-		
-		e.preventDefault();
-		
-	});
+	      var player4 = document.getElementById('player_4');
+	      $f(player4).addEvent('ready', ready);
+	     
+	      function addEvent(element, eventName, callback) {
+	        if (element.addEventListener) {
+	          element.addEventListener(eventName, callback, false)
+	        } else {
+	          element.attachEvent(eventName, callback, false);
+	        }
+	      }
+	     
+	      function ready(player_id) {
+	        var froogaloop = $f(player_id);
+	        froogaloop.addEvent('play', function(data) {
+	          $('.flexslider').flexslider("pause");
+	        });
+	        froogaloop.addEvent('pause', function(data) {
+	          $('.flexslider').flexslider("play");
+	        });
+	      }
 
-
-	/* ======= Auto play Vimeo in Bootstrpa Modal ======= */
-    
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
-    // http://stackoverflow.com/questions/24952984/play-and-stop-vimeo-video-placed-in-bootstrap-modal
-    
-    var iframe = document.getElementById('vimeo-video');
-    // $f == Froogaloop
-    var player = $f(iframe);
-    
-    $('#modal-video').on('hidden.bs.modal', function () {
-        player.api('pause');
-    });
-    
-    $('#modal-video').on('shown.bs.modal', function () {
-        player.api('play');
-    });
-
-    	/* ======= Stop Video Playing When Close the Modal Window ====== */
-    $("#modal-video .close").on("click", function() {
-        $("#modal-video iframe").attr("src", $("#modal-video iframe").attr("src"));        
-    });
-            
+	    $('.flexslider').flexslider({
+	          animation: "slide",
+	          useCSS: false,
+	          animationLoop: false,
+	          smoothHeight: true,
+	          controlNav: "thumbnails",
+	          directionNav: false,
+	          slideshow: false,
+	          before: function(slider){
+	            $f(player1).api('pause');
+	            $f(player2).api('pause');
+	            $f(player3).api('pause');
+	            $f(player4).api('pause');
+	          }
+	    });
+	}
+                   
 });
--- app/controllers/website_controller.rb
+++ app/controllers/website_controller.rb
@@ -1,10 +1,6 @@
 class WebsiteController < ApplicationController
 	def index
-		@page_body_cs = "landing-page"
-	end
-
-	def features
-		@page_body_cls = "features-page"
+		@page_body_cls = "index"
 	end
 
 	def pricing
